bb6b3c533fc63a537196cd9b9b86bd0f92b490b5
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2022-01-31 10:07:11 +0200

    fix bug with seeding not working

--- database/seeders/LikeSeeder.php
+++ database/seeders/LikeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Like;
 use App\Models\Post;
 use App\Models\User;
@@ -20,16 +21,18 @@ class LikeSeeder extends Seeder
         foreach ($posts as $post){
             $rand = rand(0,20);
             $users = User::inRandomOrder()->take($rand)->get();
-            Like::factory($rand)->make(['post_id' => $post->id])->each(function ($like, $key) use ($users){
+            Like::factory($rand)->make(['likable_id' => $post->id])->each(function ($like, $key) use ($users){
                 $like->user_id = $users[$key]->id;
+                $like->likable_type = Post::class;
                 $like->save();
             });
 
             foreach ($post->comments as $comment){
                 $rand = rand(0,20);
                 $users = User::inRandomOrder()->take($rand)->get();
-                Like::factory($rand)->make(['comment_id' => $comment->id])->each(function ($like, $key) use ($users){
+                Like::factory($rand)->make(['likable_id' => $comment->id])->each(function ($like, $key) use ($users){
                     $like->user_id = $users[$key]->id;
+                    $like->likable_type = Comment::class;
                     $like->save();
                 });
             }
